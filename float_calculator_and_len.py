b8cf80384459f96f11090176626a4e0f396797ed
Kristy LeAnn Morgan <klm3599@yahoo.com>
2021-09-26 23:50:02 -0400

    Add files via upload

---
+++ float_calculator_and_len.py
@@ -0,0 +1,39 @@
+
+print("Hi, welcome to Kristy's super awesome calculator that can only do addition!")
+print("Enter a number, any number! As long as it's an integer or a decimal. None of that weird variable algebra crap!")
+
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+
+print(result) 
+
+input('Press ENTER to continue for a surprise')
+
+
+x = str(result)
+# print(len(x))
+
+y = str(len(x)) 
+
+
+print("Your number is " + y + " characters long!")
+
+from time import sleep
+print("Which is cool...")
+sleep(1)
+print("I guess...")
+sleep(1)
+print("If you're into that sort of thing...")
+sleep(2)
+print("Weirdo.")
+sleep(3)
+
+
+input('Press ENTER to exit') 
+print("Not really though! haha!")
+sleep(1)
+print("Okay, for real, hit ENTER to leave")
+sleep(2)
+input()
